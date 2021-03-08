a106c60e6d2777f22e1dd215d6bf116694479cf3
はこだてたろう <hakodate.1106v@gmail.com>
2021-03-08 13:03:44 +0900

    add serviceworker

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -9,3 +9,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/serviceworker.js', { scope: '/' })
+    .then(function(reg) {
+      console.log('登録に成功しました。 Scope は ' + reg.scope);
+    }).catch(function(error) {
+      console.log('登録に失敗しました。' + error);
+    });
+}
